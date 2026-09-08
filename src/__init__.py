d576411104a49bb820c6182401e7738f787d4e64
Fabio Schilirò <fabio.schiliro.fs@gmail.com>
2026-09-08 18:49:54 +0200

    Init

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,17 @@
-from .utils import constants, MissingValuesAnalyzer
+from .utils import (constants, dataset_presets, model_presets, MissingValuesAnalyzer, DATASET_PRESETS, MODEL_PRESETS,
+                    DataSplit, get_model_preset, list_model_presets, compare_trainers)
+from .trainers import (ModelTrainer, CatBoostTrainer, LightGBMTrainer, XGBoostTrainer, RandomForestTrainer,
+                       GradientBoostingTrainer, LogisticRegressionTrainer, NeuralNetTrainer,)
 from .visualizer import DatasetVisualizer
 from .dataset import SpaceShipDataset
 
-__all__ = ['SpaceShipDataset', 'DatasetVisualizer', 'MissingValuesAnalyzer', 'constants']
+
+__all__ = [
+            'SpaceShipDataset', 'DatasetVisualizer', 'MissingValuesAnalyzer', 'DataSplit', 'DATASET_PRESETS',
+            'MODEL_PRESETS', 'get_model_preset', 'list_model_presets', 'constants', 'dataset_presets', 'model_presets',
+            'compare_trainers',
+            
+            'ModelTrainer', 'CatBoostTrainer', 'LightGBMTrainer', 'XGBoostTrainer', 'RandomForestTrainer',
+            'GradientBoostingTrainer', 'LogisticRegressionTrainer', 'NeuralNetTrainer',
+        
+        ]
