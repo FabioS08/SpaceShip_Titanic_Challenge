322a40647a92551f4844f2d5da0311e242cf70e5
Fabio Schilirò <fabio.schiliro.fs@gmail.com>
2026-09-08 18:46:11 +0200

    Init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,12 @@
+from .model_presets import MODEL_PRESETS, get_model_preset, list_model_presets
 from .missing_values import MissingValuesAnalyzer
+from .model_comparison import compare_trainers
+from .dataset_presets import DATASET_PRESETS
+from .data_split import DataSplit
 from . import constants
 
-__all__ = ["MissingValuesAnalyzer", "constants"]
+__all__ = [
+                "MissingValuesAnalyzer", "DataSplit",
+                "constants", "dataset_presets", "model_comparison", "DATASET_PRESETS", "MODEL_PRESETS",
+                "get_model_preset", "list_model_presets", "compare_trainers"
+            ]
